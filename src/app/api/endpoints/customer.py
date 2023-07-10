5b6d6e68a4ef29b59c3e38f1cd36364c4f66e811
Marline Khavele <marline.khavele-mmbono@sennder.com>
2023-07-10 18:21:39 +0200

    fix: Added types on customer endpoint

--- src/app/api/endpoints/customer.py
+++ src/app/api/endpoints/customer.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import AsyncGenerator
+from typing import AsyncGenerator, List
 from app.database.session import SessionLocal
 from app.schemas import(CustomerSchema,) 
 from app.models import Customer
@@ -25,11 +25,16 @@ async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
 #CRUD Operation
 
 @router.post("/customers/")
-async def create_customer(customer: CustomerSchema,db:AsyncSession = Depends(get_db_session)):
+async def create_customer(customer: CustomerSchema,db:AsyncSession = Depends(get_db_session))-> Customer:
     """
     Create a Customer and store it in the database
     """
-    new_customer=Customer(id=customer.id,name=customer.name,phone=customer.phone,email=customer.email)
+    new_customer=Customer(
+        id=customer.id,
+        name=customer.name,
+        phone=customer.phone,
+        email=customer.email
+    )
     # print(new_customer.email)
     db.add(new_customer)
     await db.commit()
@@ -37,7 +42,7 @@ async def create_customer(customer: CustomerSchema,db:AsyncSession = Depends(get
     return new_customer
   
 @router.get("/customers/")
-async def get_customers(db:AsyncSession = Depends(get_db_session)):
+async def get_customers(db:AsyncSession = Depends(get_db_session))-> List[Customer]:
     """
     Get customers that are in the database
     """
@@ -47,11 +52,11 @@ async def get_customers(db:AsyncSession = Depends(get_db_session)):
     return customers
 
 @router.get("/customers/{id}")
-async def get_customer_id(id: uuid.UUID,db:AsyncSession = Depends(get_db_session)):
+async def get_customer_id(id: uuid.UUID,db:AsyncSession = Depends(get_db_session))-> Customer:
     """
     Get customers that are in the database by id
     """
-    customer:Customer = await db.execute(select(Customer).filter(Customer.id == id))
+    customer: Customer = await db.execute(select(Customer).filter(Customer.id == id))
     #retrieves the single result row from the executed query, if any. 
     # If there are no results, it returns None.
     #This line assumes that only one customer with the given UUID exists in the database.
@@ -62,11 +67,11 @@ async def get_customer_id(id: uuid.UUID,db:AsyncSession = Depends(get_db_session
     return customer_obj
 
 @router.put("/customers/{id}")
-async def update_customer_id(id: uuid.UUID, customer: CustomerSchema, db: AsyncSession = Depends(get_db_session)):
+async def update_customer_id(id: uuid.UUID, customer: CustomerSchema, db: AsyncSession = Depends(get_db_session))-> Customer:
     """
     Update customer details using their ID that is in the database
     """
-    db_customer:Customer = await db.execute(select(Customer).filter(Customer.id == id))
+    db_customer: Customer = await db.execute(select(Customer).filter(Customer.id == id))
     #retrieves the single result row from the executed query, if any. 
     # If there are no results, it returns None.
     #This line assumes that only one customer with the given UUID exists in the database.
@@ -85,11 +90,11 @@ async def update_customer_id(id: uuid.UUID, customer: CustomerSchema, db: AsyncS
 
 
 @router.delete("/customers/{id}")
-async def delete_customer_id(id: uuid.UUID, db: AsyncSession = Depends(get_db_session)):
+async def delete_customer_id(id: uuid.UUID, db: AsyncSession = Depends(get_db_session))-> Customer:
     """
     Delete customer details using their UUID that is stored in the database
     """
-    customer:Customer = await db.execute(select(Customer).filter(Customer.id == id))
+    customer: Customer = await db.execute(select(Customer).filter(Customer.id == id))
     #retrieves the single result row from the executed query if any. I
     # f there are no results, it returns None.
     customer_obj = customer.scalar_one_or_none()
