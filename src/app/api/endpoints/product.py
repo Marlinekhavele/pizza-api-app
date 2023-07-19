aab0b816d2a9435c774fe5037cdc9dbbdec37253
Marline Khavele <marline.khavele-mmbono@sennder.com>
2023-07-19 05:29:45 +0200

    feat: added product size repositories

--- src/app/api/endpoints/product.py
+++ src/app/api/endpoints/product.py
@@ -5,7 +5,6 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.deps import get_db_session
-from app.models import Product, ProductFlavour, ProductSize
 from app.repositories.product_repositories import ProductRepository
 from app.schemas import ProductFlavourSchema, ProductSchema, ProductSizeSchema
 
@@ -103,7 +102,7 @@ async def get_products_flavours_id(
 async def update_products_flavours_id(
     id: uuid.UUID,
     product_flavour: ProductFlavourSchema,
-    repo: ProductRepository = Depends(ProductRepository)
+    repo: ProductRepository = Depends(ProductRepository),
 ):
     """
     Update products flavours details using ID's that are stored  database, here
@@ -131,78 +130,62 @@ async def delete_products_flavours_id(
 # CRUD productsize
 @router.post("/products/sizes")
 async def create_products_sizes(
-    product_size: ProductSizeSchema, db: AsyncSession = Depends(get_db_session)
+    product_size: ProductSizeSchema, product_size_repo: ProductRepository = Depends(ProductRepository),
 ):
     """
     Create a products sizes and store it in the database
     """
-    new_product_size = ProductSize(
-        id=product_size.id, active=product_size.active, product_id=product_size.product_id
-    )
-    # print(new_product_size.active)
-    db.add(new_product_size)
-    await db.commit()
-    await db.refresh(new_product_size)
+    new_product_size = await product_size_repo.create_product_flavours(product_size)
     return new_product_size
 
 
-# debug
 @router.get("/products/sizes")
-async def get_products_size(db: AsyncSession = Depends(get_db_session)):
+async def get_products_size(product_size_repo: ProductRepository = Depends(ProductRepository)):
     """
     Get  all product sizes that are in the database
     """
-    results = await db.execute(select(ProductSize))
-    print(results)
-    # This method retrieves all the objects from the query result set and returns them as a list.
-    product_sizes = results.scalars().all()
-    return product_sizes
+    return await product_size_repo.get_products_sizes()
+
 
 
 @router.get("/products/{id}/sizes")
 async def get_products_sizes_id(
-    id: uuid.UUID, db: AsyncSession = Depends(get_db_session)
+    id: uuid.UUID, product_size_repo: ProductRepository = Depends(ProductRepository)
 ):
     """
     Get products sizes  by id here
     """
-    product_size = await db.execute(select(ProductSize).filter(ProductSize.id == id))
-    product_size_obj = product_size.scalar_one_or_none()
-    return product_size_obj
+    return await product_size_repo.get_product_size_by_id(id)
+
+  
 
 
 @router.put("/products/{id}/sizes")
 async def update_products_sizes_id(
     id: uuid.UUID,
     product_size: ProductSizeSchema,
-    db: AsyncSession = Depends(get_db_session),
+        repo: ProductRepository = Depends(ProductRepository),
 ):
     """
     Update products sizes details using ID's that are stored  database, here
     - we can update a field like:
     - title
     """
-    db_product_size = await db.execute(select(ProductSize).filter(ProductSize.id == id))
-    product_size_obj = db_product_size.scalar_one_or_none()
+    updated_product_size = await repo.update_product_size(
+        id,
+        product_size.title
+    )
+    return updated_product_size
 
-    if product_size_obj:
-        product_size_obj.title = product_size.title
-        await db.commit()
-
-    return product_size_obj
 
 
 @router.delete("/products/{id}/sizes")
 async def delete_products_sizes_id(
-    id: uuid.UUID, db: AsyncSession = Depends(get_db_session)
+    id: uuid.UUID,repo: ProductRepository = Depends(ProductRepository)
 ):
     """
     Delete product sizes
     """
-    product_size = await db.execute(select(ProductSize).filter(ProductSize.id == id))
-    product_size_obj = product_size.scalar_one_or_none()
-
-    if product_size_obj:
-        db.delete(product_size_obj)
-        await db.commit()
-    return product_size
+    delete_product_size = await repo.delete_product_size(id)
+    return delete_product_size
+    
--- src/app/repositories/product_repositories.py
+++ src/app/repositories/product_repositories.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.exc import NoResultFound
 from app.deps import get_db_session
-from app.models import Product, ProductFlavour
+from app.models import Product, ProductFlavour, ProductSize
 
 
 class ProductRepository:
@@ -102,8 +102,46 @@ class ProductRepository:
             await self.db.commit()
         return product_flavour
     
-
-
-
-
     # sizes
+    async def create_product_sizes(self, product_size_data):
+        new_product_size = ProductSize(
+        id=product_size_data.id,
+        title=product_size_data.title,
+        active=product_size_data.active,
+        product_id=product_size_data.product_id,
+        )
+        self.db.add(new_product_size)
+        await self.db.commit()
+        await self.db.refresh(new_product_size)
+        return new_product_size
+    
+    async def get_products_sizes(self):
+        results = await self.db.execute(select(ProductSize))
+        products_sizes = results.scalars().all()
+        return products_sizes
+
+    async def get_product_size_by_id(self, product_size_id: uuid.UUID):
+        try:
+            query = select(ProductSize).filter(ProductSize.id == product_size_id)
+            result = await self.db.execute(query)
+            product_size_obj =  result.scalar_one()
+            return product_size_obj
+        except NoResultFound:
+            return None
+
+    async def update_product_size(
+        self, product_size_id: uuid.UUID, title: str,
+    ):
+        product_size_obj = await self.get_product_flavour_by_id(product_size_id)
+        if product_size_obj:
+            product_size_obj.title = title
+            await self.db.commit()
+        return product_size_obj
+    
+
+    async def delete_product_size(self, product_size_id: uuid.UUID):
+        product_size = await self.get_product_by_id(product_size_id)
+        if product_size:
+            self.db.delete(product_size)
+            await self.db.commit()
+        return product_size
